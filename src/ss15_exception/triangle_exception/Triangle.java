4ecfef580b68a634083926ffdccd58d79de4ab52
sffs761 <cuong02ht@gmail.com>
2025-01-03 20:49:07 +0700

    modified

--- src/ss15_exception/triangle_exception/Triangle.java
+++ src/ss15_exception/triangle_exception/Triangle.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 
 public class Triangle {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        try {
+        try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Nhập cạnh thứ nhất: ");
             double a = Double.parseDouble(scanner.nextLine());
             System.out.print("Nhập cạnh thứ hai: ");
@@ -16,10 +15,9 @@ public class Triangle {
             System.out.println("Ba cạnh của tam giác hợp lệ.");
         } catch (Exception e) {
             System.out.println("Vui lòng nhập số hợp lệ.");
-        } finally {
-            scanner.close();
         }
     }
+
     public static void validateTriangle(double a, double b, double c) throws IllegalTriangleException {
         if (a < 0 || b < 0 || c < 0) {
             throw new IllegalTriangleException("Cạnh của tam giác phải là số dương.");
